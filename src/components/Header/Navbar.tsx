ba9fc8c0d04c64f21fd03b0eed37ae367cb36621
Renan Gomes <renango2015@gmail.com>
2025-02-28 05:21:49 -0300

    style: update navbar 'z-index'

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -2,7 +2,7 @@
 
 export function Navbar() {
   return (
-    <header className='sticky top-0 z-50 border-b bg-white px-4 py-4 sm:px-10 lg:px-20 xl:px-40 2xl:px-60'>
+    <header className='sticky top-0 z-[999] border-b bg-white px-4 py-4 sm:px-10 lg:px-20 xl:px-40 2xl:px-60'>
       <span className='font-saira-stencil text-4xl text-zinc-600'>FakeStoreAPI</span>
     </header>
   );
